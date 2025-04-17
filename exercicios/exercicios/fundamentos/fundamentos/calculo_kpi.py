597dd794f27d2f26a80a82c18b278bd4f554a704
Rafael Alvarenga <devrafaelalvarenga@gmail.com>
2025-04-17 00:13:19 -0300

    update identificar_desempenho()

--- exercicios/exercicios/fundamentos/fundamentos/calculo_kpi.py
+++ exercicios/exercicios/fundamentos/fundamentos/calculo_kpi.py
@@ -3,11 +3,11 @@
     o valor do seu salário mensal e o valor do bônus que recebeu. 
     O programa deve, então, imprimir uma mensagem saudando o usuário pelo nome e 
     informando o valor do salário em comparação com o bônus recebido.
-    '''
+'''
 
 
 def coletar_nome() -> str:
-    """Função que coleta o nome do usuario. 
+    """Função que coleta o nome informado pelo usuario. 
        Programa é encerrado após três tentativas inválidas.
 
     Raises:
@@ -27,13 +27,95 @@ def coletar_nome() -> str:
             print(e)
             validacao += 1
             if validacao == 3:
-                print(f'Tente novamente')
+                print(f'Número de tentativas excedido. Tente novamente')
                 exit()
             else:
-                print(f'Tentativa: {validacao}')
+                print(f'Tentativas restantes: {3 - validacao}')
         else:
             return nome_usuario
 
 
+def coletar_salario() -> float:
+    """Função que coleta o valor do salário informado pelo usuário. 
+       Programa é encerrado após três tentativas inválidas.
+
+    Raises:
+        ValueError: Informe um valor valido e "inteiro" para o salário.
+        ValueError: Informe um valor valido e maior que zero para o salário.
+
+    Returns:
+        float: Função retorna o valor do salário informado pelo usuário
+    """
+    validacao: int = 0
+    while validacao < 3:
+        try:
+            salario_usuario = float(
+                input('Digite o valor "inteiro" seu salário: R$'))
+            if salario_usuario.is_integer() == False:
+                raise ValueError(
+                    'Informe um valor valido e "inteiro" para o salário.')
+            elif salario_usuario <= 0:
+                raise ValueError(
+                    'Informe um valor valido e maior que zero para o salário.')
+        except Exception as e:
+            print(e)
+            validacao += 1
+            if validacao == 3:
+                print(f'Número de tentativas excedido. Tente novamente')
+                exit()
+            else:
+                print(f'Tentativas restantes: {3 - validacao}')
+        else:
+            return salario_usuario
+
+
+def identificar_desempenho() -> float:
+    """Função que gera o multiplicador utilizado no cálculo do bonus do usuário.
+       Programa é encerrado após três tentativas inválidas.
+
+    Raises:
+        ValueError: Informe um nível de desempenho válido (um número entre 1 e 5).
+
+    Returns:
+        float: Valor do multiplicador utilizado no cálculo do bonus do usuário
+    """
+    validacao: int = 0
+    while validacao < 3:
+        try:
+            entrada: str = input(
+                'Informe o número que represente o nível de desempenho informado na sua avaliação: 1-Excelente | 2-Muito Bom | 3-Bom | 4-Regular | 5-Insatisfatório : ')
+            if entrada.isnumeric() == False:
+                raise ValueError(
+                    'Informe um nível de desempenho válido (um número entre 1 e 5).')
+            elif int(entrada) not in range(1, 6):
+                raise ValueError(
+                    'Informe um nível de desempenho válido (um número entre 1 e 5).')
+
+            nivel_de_desempenho = int(entrada)
+            if nivel_de_desempenho == 1:
+                multiplicador = 2.0
+            elif nivel_de_desempenho == 2:
+                multiplicador = 1.5
+            elif nivel_de_desempenho == 3:
+                multiplicador = 1.0
+            elif nivel_de_desempenho == 4:
+                multiplicador = 0.5
+            elif nivel_de_desempenho == 5:
+                multiplicador = 0.0
+
+        except Exception as e:
+            print(e)
+            validacao += 1
+            if validacao == 3:
+                print(f'Número de tentativas excedido. Tente novamente')
+                exit()
+            else:
+                print(f'Tentativas restantes: {3 - validacao}')
+        else:
+            return multiplicador
+
+
 if __name__ == '__main__':
     coletar_nome()
+    coletar_salario()
+    identificar_desempenho()
